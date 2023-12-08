aef540338616bfec2ef4b7dd7dc11415d33613ad
HugoALeuchs <hugoandradeleuchs@hotmail.com>
2023-12-08 19:00:10 -0300

    Add the Portfolio project

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,134 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import styles from "./page.module.css";
+import Spline from "@splinetool/react-spline";
+import { TypeAnimation } from "react-type-animation";
+import { useEffect, useState } from "react";
+import { TaggedContentCard } from "react-ui-cards";
+import ReactProgressMeter from "react-progress-meter";
 
 export default function Home() {
+  const [loaded, setLoading] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let auxCount = 0;
+    const interval = setInterval(() => {
+      auxCount += 1;
+      setCounter(auxCount);
+      if (auxCount > 100) {
+        setLoading(true);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      {loaded ? (
+        <>
+          <p className={styles.contact}>
+            <a href="mailto:hugoandradeleuchs@hotmail.com">
+              hugoandradeleuchs@hotmail.com
+            </a>
+            <br />
+            <a
+              href="https://api.whatsapp.com/send?l=pt_br&phone=5544998896630"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "black" }}
+            >
+              +55 (44) 9 9889-6630
+            </a>
           </p>
-        </a>
-      </div>
+          <TypeAnimation
+            sequence={[
+              "Hi!\n",
+              100,
+              "Hi!\nI'm Hugo Leuchs \n",
+              100,
+              "Hi!\nI'm Hugo Leuchs \nWeb/Front-End Developer",
+              100,
+            ]}
+            className={styles.heroText}
+            repeat={0}
+          />
+          <div className={styles.textBlock}>
+            Bachelor in Computer Engineering
+            <br />
+            4 years as a Web/Front-End Developer
+            <br />
+            Highly experienced in Next.js
+            <br />
+            Experienced building creative and
+            <br />
+            pixel-perfect websites
+          </div>
+          <h3 className={styles.companiesText}>
+            Companies that I've worked on:
+          </h3>
+          <div className={styles.portifolioGrid}>
+            <TaggedContentCard
+              float
+              href="https:loadsmart.com"
+              thumbnail="/images/LoadsmartHomePage.png"
+              title="Loadsmart"
+              description="I worked on creating new pages and maintaining old ones and also helped to migrate the website from a monolith project to a Next.js project."
+              tags={[]}
+              className={styles.card}
+            />
+            <TaggedContentCard
+              float
+              href="https://loadsmart.com/shipper/shipperguide/"
+              thumbnail="/images/LoadsmartShipperGuide.png"
+              title="Loadsmart ShipperGuide"
+              description="I participated in all stages of the development of this page, from design discussions to coding, helping with development decision-making."
+              tags={[]}
+              className={styles.card}
+            />
+            <TaggedContentCard
+              float
+              href="https://loja.bariguiseminovos.com.br/"
+              thumbnail="/images/NovoVarejo.png"
+              title="NovoVarejo E-Commerce"
+              description="I helped with the development of new features for the company's E-Commerce platform, mainly using React with GraphQL."
+              tags={[]}
+              className={styles.card}
+            />
+            <TaggedContentCard
+              float
+              href="https://biopark.com.br"
+              thumbnail="/images/Biopark.png"
+              title="Biopark"
+              description="I led the website development, participating in decision-making together with the marketing team and the board to make this website from scratch."
+              tags={[]}
+              className={styles.card}
+            />
+          </div>
+        </>
+      ) : (
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            width: "50%",
+            display: "inline-block",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "20px",
+          }}
+        >
+          <ReactProgressMeter
+            currentProgress={counter}
+            showPercent={true}
+            color="cyan"
+          />
+        </div>
+      )}
+      <Spline
+        scene="https://prod.spline.design/3y2cbA4TrOjBFPEX/scene.splinecode"
+        style={{ visibility: loaded ? "visible" : "hidden", position: "fixed" }}
+      />
     </main>
-  )
+  );
 }
